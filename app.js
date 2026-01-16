2022d6dbaef8306575a5216c76615f5a5b7c8b07
Ayan-004 <ayyanjsk@gmail.com>
2026-01-16 22:10:35 +0530

    Made API endpoint to get and add new data

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 module.exports = app
 
+app.use(bodyParser.json())
+
+
+const envelopes = []
+let nextId = 1;
+
 app.get('/', (req, res, next) => {
-    res.send("Hello World")
+    res.send(envelopes)
+})
+
+app.post('/envelopes', (req, res, next) => {
+    const newData = req.body;
+    
+    if (newData.title && newData.budget) {
+        newData.id = nextId;
+        nextId++;
+        
+        envelopes.push(newData)
+        res.send(newData)
+    } else {
+        res.status(400).send("Make sure you include title and budget in the request")
+    }
+})
+
+app.get('/envelopes/:id', (req, res, next) => {
+    const envelopId = Number(req.params.id)
+    const envelopeById = envelopes.filter(envelope => {
+        return envelope.id === envelopId
+    })
+
+    res.send(envelopeById)
 })
